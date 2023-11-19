f2458fac2523a936e9f93062d95623997a28fed7
Edwin Hernandez <Edwinhern.16@gmail.com>
2023-11-19 00:16:53 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from pytube import YouTube
+import os
+
+
+def download_video(url, destination):
+    try:
+        yt = YouTube(url)
+
+        # Extract only audio
+        video = yt.streams.filter(only_audio=True).first()
+
+        # Download the file
+        out_file = video.download(output_path=destination)
+
+        # Save the file
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+
+        print(yt.title + " has been successfully downloaded.")
+    except Exception as e:
+        print(f"Failed to download {url}. Error: {e}")
+        # Continue with other downloads even if this one fails
+
+
+def main():
+    urls = []
+    print("Enter YouTube URLs (press enter on an empty line to start download):")
+
+    while True:
+        url = input(">> ")
+        if url:
+            urls.append(url)
+        else:
+            print("No more URLs entered. Starting download...")
+            break
+
+    # Replace the string below with the path to the folder where you want to save the downloads
+    # Example: destination = "C:/Users/YourUsername/Downloads"
+    destination = "/path/to/your/destination/folder"
+
+    # Check if destination folder exists, if not create it
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    for url in urls:
+        download_video(url, destination)
+
+
+if __name__ == "__main__":
+    main()
